5fafb7ddeb3681d052741cc8ea18ecff4a5893e5
LasseHindsberg <59876303+FrostedHazel@users.noreply.github.com>
2021-01-15 13:10:21 +0100

    image for "arthor"

---
+++ src/components/CollectionProfileImage.js
@@ -0,0 +1,13 @@
+// CollectionProfileImage.js
+import React from "react";
+import './Collection.css';
+
+function CollectionProfileImage() {
+    return (
+        <>
+        <img className="collection__profileImage" src='https://picsum.photos/60/60' alt='' />
+        </>
+    )
+}
+
+export default CollectionProfileImage;
